88a01aaa21efecbb3eebf64692ed87e8163f1879
RezaMagnet <rezakhonsari@icloud.com>
2020-06-09 13:12:16 +0430

    handle all dependencies & working package

--- Sources/CollectionKitPackage/CollectionKitPackage.swift
+++
@@ -1,3 +0,0 @@
-struct CollectionKitPackage {
-    var text = "Hello, World!"
-}
--- Tests/CollectionKitPackageTests/CollectionKitPackageTests.swift
+++ Tests/CollectionKitPackageTests/CollectionKitPackageTests.swift
@@ -3,10 +3,8 @@ import XCTest
 
 final class CollectionKitPackageTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CollectionKitPackage().text, "Hello, World!")
+ 
+        
     }
 
     static var allTests = [
